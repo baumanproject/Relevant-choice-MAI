5fc19f125233b49b3d875b490bd36db6ebaed43a
Alexander <ledgergap@gmail.com>
2020-11-16 21:21:15 +0300

    improve file paths

--- rund.sh
+++ rund.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8080:8080 -v home/ubuntu/PDF/Relevant-choice-MAI/logs:/opt/lib/logs pdf_reader
+docker run -d -p 8080:8080 -v /home/ubuntu/PDF/Relevant-choice-MAI/logs:/opt/lib/logs pdf_reader
